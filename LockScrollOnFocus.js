a1aa1e77ee90ba838d24b436829b6d6c0ed71723
Ismorod <197842746+Ismorod@users.noreply.github.com>
2025-06-12 06:31:39 +0200

    Added a functioning scroll lock script.

--- LockScrollOnFocus.js
+++
@@ -1,6 +0,0 @@
-window.visualViewport.addEventListener('resize',()=> {
-    const activeElement = document.activeElement;
-    if (activeElement && ['INPUT','TEXTAREA'].includes(activeElement.tagName)) {
-        activeElement.scrollIntoView({behavior: 'smooth',block:'center'});
-    }
-});
