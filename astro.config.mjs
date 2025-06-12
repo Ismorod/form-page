a1a162d4c7cc91f56af0be59fe8e8df89f39df9e
Ismorod <197842746+Ismorod@users.noreply.github.com>
2025-06-12 05:02:07 +0200

    Another config test

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,7 +3,7 @@ import { defineConfig } from 'astro/config';
 
 // https://astro.build/config
 export default defineConfig({
-    site: 'https://ismorod.github.io/form-page/',
+    site: 'https://ismorod.github.io/',
     base: '/form-page',
     output: 'static'
 });
